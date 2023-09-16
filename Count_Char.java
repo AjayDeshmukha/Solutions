45fb5a14e5c52fa421e46c0dc92f1530e978f622
Ajay <120307618+AjayDeshmukha@users.noreply.github.com>
2023-09-16 20:54:10 +0530

    Add files via upload

---
+++ Count_Char.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Count_Char {
+
+	/* Program 1: Program to count occurrences of a Given character in a string */
+
+	public void count() {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		char e = 'e';
+		char a = 'a';
+		int count_e = 0;
+		int count_a = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (e == str.charAt(i)) {
+				count_e++;
+			} else if (a == str.charAt(i)) {
+				count_a++;
+			}
+
+		}
+		System.out.println(count_e);
+		System.out.println(count_a);
+	}
+
+	public static void main(String[] args) {
+		Count_Char obb = new Count_Char();
+		obb.count();
+
+	}
+
+}
