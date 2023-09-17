4ad5af3f6a99e758af77fc6e447612dc6e3484ff
ajay <ajaydeshmukha@1998gmail.com>
2023-09-17 16:54:47 +0530

    Problem 5: Swap Two Number Without using a temporary variable

--- SwapTwoNumber.java
+++ SwapTwoNumber.java
@@ -3,7 +3,7 @@ public class SwapTwoNumber {
 
 	public static void main(String[] args) {
 
-		/* Problem 5: Swap Two Number Without using a temporary variable */
+		/* Problem 5: Swap Two Number Without using a temporary variable  */
 
 		int a = 10;
 		int b = 50;
